35ce67cb2c30ecbbac7876a1124c88d36b96a9dd
Hajime Hoshi <hajimehoshi@gmail.com>
2018-06-24 02:01:17 +0900

    Fix js.ValueOf to create typed arrays

--- js/js_wasm.go
+++ js/js_wasm.go
@@ -50,37 +50,59 @@ type Error = js.Error
 
 type Value = js.Value
 
+var (
+	int8Array    = js.Global.Get("Int8Array")
+	int16Array   = js.Global.Get("Int16Array")
+	int32Array   = js.Global.Get("Int32Array")
+	int64Array   = js.Global.Get("Int64Array")
+	uint16Array  = js.Global.Get("Uint16Array")
+	uint32Array  = js.Global.Get("Uint32Array")
+	uint64Array  = js.Global.Get("Uint64Array")
+	float32Array = js.Global.Get("Float32Array")
+	float64Array = js.Global.Get("Float64Array")
+)
+
 func ValueOf(x interface{}) Value {
 	var xh *reflect.SliceHeader
+	var class js.Value
 	size := 0
 	switch x := x.(type) {
 	case []int8:
 		size = 1
 		xh = (*reflect.SliceHeader)(unsafe.Pointer(&x))
+		class = int8Array
 	case []int16:
 		size = 2
 		xh = (*reflect.SliceHeader)(unsafe.Pointer(&x))
+		class = int16Array
 	case []int32:
 		size = 4
 		xh = (*reflect.SliceHeader)(unsafe.Pointer(&x))
+		class = int32Array
 	case []int64:
 		size = 8
 		xh = (*reflect.SliceHeader)(unsafe.Pointer(&x))
+		class = int64Array
 	case []uint16:
 		size = 2
 		xh = (*reflect.SliceHeader)(unsafe.Pointer(&x))
+		class = uint16Array
 	case []uint32:
 		size = 4
 		xh = (*reflect.SliceHeader)(unsafe.Pointer(&x))
+		class = uint32Array
 	case []uint64:
 		size = 8
 		xh = (*reflect.SliceHeader)(unsafe.Pointer(&x))
+		class = uint64Array
 	case []float32:
 		size = 4
 		xh = (*reflect.SliceHeader)(unsafe.Pointer(&x))
+		class = float32Array
 	case []float64:
 		size = 8
 		xh = (*reflect.SliceHeader)(unsafe.Pointer(&x))
+		class = float64Array
 	default:
 		return js.ValueOf(x)
 	}
@@ -90,7 +112,9 @@ func ValueOf(x interface{}) Value {
 	bh.Data = xh.Data
 	bh.Len = xh.Len * size
 	bh.Cap = xh.Cap * size
-	return js.ValueOf(b)
+
+	u8 := js.ValueOf(b)
+	return class.New(u8.Get("buffer"), u8.Get("byteOffset"), xh.Len)
 }
 
 func GetInternalObject(v Value) interface{} {
